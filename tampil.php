ff921c8f39b37085b4ec5e87f02c204ed77bbfa4
KennaN230 <adityabw230@gmail.com>
2023-12-04 00:58:06 +0700

    Update tampil.php

--- tampil.php
+++ tampil.php
@@ -10,23 +10,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Check if the query was successful
     if (!$result) {
-        die("Query failed: " . $conn->error);
-    }
-
-    // Check if there are rows in the result set
-    if ($result->num_rows > 0) {
-        // Fetch data and display
-        while ($row = $result->fetch_assoc()) {
-            echo "Nama Produk: " . $row['NProduk'] . "<br>";
-            echo "Harga Jual: " . $row['HJual'] . "<br>";
-            echo "<br>";
-        }
+        $response = array('status' => 'error', 'message' => 'Query failed: ' . $conn->error);
+        echo json_encode($response);
     } else {
-        echo "No records found";
-    }
+        // Check if there are rows in the result set
+        if ($result->num_rows > 0) {
+            // Fetch data and build an array
+            $data = array();
+            while ($row = $result->fetch_assoc()) {
+                $data[] = array(
+                    'NamaProduk' => $row['NProduk'],
+                    'HargaJual' => $row['HJual']
+                );
+            }
 
-    // Close the result set
-    $result->close();
+            // Send JSON response
+            $response = array('status' => 'success', 'data' => $data);
+            echo json_encode($response);
+        } else {
+            $response = array('status' => 'error', 'message' => 'No records found');
+            echo json_encode($response);
+        }
+
+        // Close the result set
+        $result->close();
+    }
 }
 
 // Close the connection when you're done using it
