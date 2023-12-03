6b1d18795e35c125ce9526108a159827cefb9a68
KennaN230 <adityabw230@gmail.com>
2023-12-04 06:00:54 +0700

    Update tampil.php

--- tampil.php
+++ tampil.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
 }
 
 // Query untuk mengambil data dari tabel (ganti 'nama_tabel' dengan nama tabel yang sesuai)
-$sql = "SELECT NProduk, HJual FROM produk";
+$sql = "SELECT NProduk, HJual, gambarproduk FROM produk";
 $result = $conn->query($sql);
 
 // Memeriksa apakah query berhasil dijalankan
@@ -26,6 +26,7 @@ if ($result->num_rows > 0) {
         echo "<tr>
                 <td>" . $row["NProduk"] . "</td>
                 <td>" . $row["HJual"] . "</td>
+                <td><img src='" . $row["gambarproduk"] . "' alt='Gambar Produk'></td>
               </tr>";
     }
 
