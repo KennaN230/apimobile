22fd9d45b21310c86cff6bbdf68d7f6878a78f59
KennaN230 <adityabw230@gmail.com>
2023-12-04 08:36:58 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,17 +1,34 @@
 <?php
-$localhostUrl = 'http://10.10.180.71//Griya/login.php'; // Ganti dengan URL server lokal Anda.
+require_once 'koneksi.php'; // Adjust according to your database connection file
 
-$context = stream_context_create(array(
-    'http' => array(
-        'timeout' => 5, // Waktu maksimum (dalam detik) untuk menunggu respons.
-    )
-));
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Membuat koneksi di sini agar tetap terbuka selama operasi
+    $conn = new mysqli($host, $username, $password, $database);
 
-$response = file_get_contents($localhostUrl, false, $context);
+    // Menangani kesalahan koneksi
+    if ($conn->connect_error) {
+        die("Koneksi gagal: " . $conn->connect_error);
+    }
 
-if ($response !== false) {
-    echo 'Koneksi ke localhost berhasil';
-} else {
-    echo 'Koneksi ke localhost gagal';
+    // Assuming 'Bawal' is payment amount and 'Titem' is quantity, modify these accordingly
+    $paymentAmount = mysqli_real_escape_string($conn, $_POST['Bawal']);
+    $quantity = mysqli_real_escape_string($conn, $_POST['Titem']);
+
+    // Insert data into the database for pemesanan
+    $insertQuery = "INSERT INTO pemesanan (IdPemesanan, PaymentAmount, Quantity) VALUES (NULL, ?, ?)";
+    $insertStmt = mysqli_prepare($conn, $insertQuery);
+    mysqli_stmt_bind_param($insertStmt, "ss", $paymentAmount, $quantity);
+
+    if (mysqli_stmt_execute($insertStmt)) {
+        $response = array('status' => 'success', 'message' => 'Order placed successfully');
+    } else {
+        $response = array('status' => 'error', 'message' => 'Order placement failed');
+    }
+
+    // Send the JSON response
+    echo json_encode($response);
+
+    // Close the prepared statements
+    mysqli_stmt_close($insertStmt);
 }
 ?>
