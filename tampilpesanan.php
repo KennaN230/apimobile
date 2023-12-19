46a74324bfc7bd6d6d977493f1b237f39b4d1d60
KennaN230 <adityabw230@gmail.com>
2023-12-20 03:55:01 +0700

    Update tampilpesanan.php

--- tampilpesanan.php
+++ tampilpesanan.php
@@ -2,41 +2,44 @@
 // Sertakan file koneksi atau kodenya sesuai dengan kebutuhan
 require_once 'koneksilokal.php';
 
-// Ambil username dari sesi atau cara lainnya sesuai dengan aplikasi Anda
-$username = "Select NKonsumen"; // Ganti dengan cara Anda mendapatkan username
+// Membuat koneksi ke database
+$conn = new mysqli($host, $user, $password, $database);
 
-// Query untuk mengambil pesanan berdasarkan username
-$sql = "SELECT id_pesanan, nama_produk, harga, qty, harga * qty AS total_harga
-        FROM detail pesanan
-        WHERE username = '$username'  from konsumen";
+// Menangani kesalahan koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+// Query untuk mengambil seluruh data dari tabel pemesanan
+$sql = "SELECT * FROM pemesanan";
 
 $result = $conn->query($sql);
 
 // Menampilkan hasil query
 if ($result->num_rows > 0) {
-    echo '<h2>Daftar Pesanan untuk ' . $username . '</h2>';
+    echo '<h2>Daftar Pesanan</h2>';
     echo '<table border="1">
             <tr>
-                <th>ID Pesanan</th>
-                <th>Nama Produk</th>
-                <th>Harga</th>
+                <th>No Pesanan</th>
+                <th>Tanggal Pemesanan</th>
+                <th>Tipe Pembayaran</th>
                 <th>Qty</th>
                 <th>Total Harga</th>
             </tr>';
 
     while ($row = $result->fetch_assoc()) {
         echo '<tr>
-                <td>' . $row['id_pesanan'] . '</td>
-                <td>' . $row['nama_produk'] . '</td>
-                <td>' . $row['harga'] . '</td>
+                <td>' . $row['NoPesanan'] . '</td>
+                <td>' . $row['TglPemesanan'] . '</td>
+                <td>' . $row['TPembayaran'] . '</td>
                 <td>' . $row['qty'] . '</td>
-                <td>' . $row['total_harga'] . '</td>
+                <td>' . $row['harga'] * $row['qty'] . '</td>
             </tr>';
     }
 
     echo '</table>';
 } else {
-    echo 'Tidak ada pesanan untuk ' . $username;
+    echo 'Tidak ada data pesanan.';
 }
 
 // Menutup koneksi
