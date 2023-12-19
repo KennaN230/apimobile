a08918e3ce4a7cb2f002d874d6d7b4134a1e1825
KennaN230 <adityabw230@gmail.com>
2023-12-20 05:47:00 +0700

    Update updateperson.php

--- updateperson.php
+++ updateperson.php
@@ -1,37 +1,35 @@
 <?php
-header("Content-Type: application/json");
 
-include 'koneksi.php';
+require_once 'koneksi.php';
 
-// Handle POST request
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Ambil data dari body request
-    $postData = file_get_contents("php://input");
-    $data = json_decode($postData);
+// Check if all required parameters are set
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['username']) && isset($_POST['AlamatKonsumen']) && isset($_POST['NoTelp']) && isset($_POST['Email'])) {
+    $conn = new mysqli($host, $username, $password, $database);
 
-    // Pastikan data diterima dengan benar
-    if ($data && isset($data->old_recipient_name) && isset($data->recipient_phone) && isset($data->full_address)) {
-        $oldRecipientName = $data->old_recipient_name;
-        $recipientPhone = $data->recipient_phone;
-        $fullAddress = $data->full_address;
-
-        // Perbarui data di database berdasarkan nama penerima
-        $query = "UPDATE konsumen SET NoTelp = '$recipientPhone', Email = '$fullAddress' WHERE NKonsumen = '$oldRecipientName'";
-        $result = $koneksi->query($query);
-
-        if ($result) {
-            $response = array("success" => true);
-        } else {
-            $response = array("success" => false);
-        }
-
-        echo json_encode($response);
-    } else {
-        // Data tidak lengkap
-        echo json_encode(array("success" => false, "message" => "Data tidak lengkap"));
+    if ($conn->connect_error) {
+        die("Koneksi gagal: " . $conn->connect_error);
     }
+
+    // Sanitize input data
+    $name = ($_POST['username']);
+    $address = ($_POST['AlamatKonsumen']);
+    $phoneNumber = ($_POST['NoTelp']);
+    $email = ($_POST['Email']);
+
+    // Update the user in the database
+    $sql = "UPDATE konsumen SET AlamatKonsumen = '$address', NoTelp = '$phoneNumber', Email = '$email' WHERE name = '$name'";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "User updated successfully";
+    } else {
+        echo "Error during update: " . $conn->error;
+    }
+
 } else {
-    // Metode request tidak diizinkan
-    echo json_encode(array("success" => false, "message" => "Metode request tidak diizinkan"));
+    echo "Missing required parameters";
 }
+
+// Close the database connection
+$conn->close();
+
 ?>
