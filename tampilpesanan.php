71fe31ef7bef8c68808ab0f7eade6cfafc95abc9
KennaN230 <adityabw230@gmail.com>
2023-12-20 04:44:41 +0700

    Update tampilpesanan.php

--- tampilpesanan.php
+++ tampilpesanan.php
@@ -17,7 +17,39 @@ $result = $conn->query($sql);
 
 // Menampilkan hasil query
 if ($result->num_rows > 0) {
+    echo '<html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <style>
+                    table {
+                        font-family: Arial, sans-serif;
+                        border-collapse: collapse;
+                        width: 100%;
+                        margin-top: 20px;
+                    }
+                    th, td {
+                        border: 1px solid #dddddd;
+                        text-align: left;
+                        padding: 8px;
+                    }
+                    th {
+                        background-color: #f2f2f2;
+                    }
+                </style>
+            </head>
+            <body>';
+
     echo '<h2>Daftar Pesanan</h2>';
+    echo '<table>';
+    echo '<tr>
+            <th>No Pesanan</th>
+            <th>Tanggal Pemesanan</th>
+            <th>Bawal</th>
+            <th>Total Item</th>
+            <th>Tipe Pembayaran</th>
+        </tr>';
+
     while ($row = $result->fetch_assoc()) {
         echo '<tr>
                 <td>' . $row['NoPesanan'] . '</td>
@@ -29,6 +61,7 @@ if ($result->num_rows > 0) {
     }
 
     echo '</table>';
+    echo '</body></html>';
 } else {
     echo 'Tidak ada data pesanan.';
 }
